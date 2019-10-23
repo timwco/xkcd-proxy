9e7bed4ac1d8c35f86edc81a147a96c3a36165eb
Tim Whitacre <tim@timw.co>
2019-10-23 11:43:38 -0400

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require('axios');
+const app = express();
+const cors = require('cors');
+
+const DOMAIN = 'https://xkcd.com'
+const PATH = 'info.0.json'
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT, () => console.log('Server Running..'));
+
+const webpage = `
+  <h1>XKCD API Proxy</h1>
+  <p>Documentation:</p>
+  <ul>
+    <li>Latest: <a href="https://xkcd.now.sh/?comic=latest"><pre>https://xkcd.now.sh/?comic=latest</pre></a></li>
+    <li>Comic #522 (and so on...): <a href="https://xkcd.now.sh/?comic=522"><pre>https://xkcd.now.sh/?comic=522</pre></a></li>
+  </ul>
+`;
+
+// Latest Comic
+app.get("/", async (req, res) => {
+  const comic = req.query.comic;
+  let response = webpage;
+  if (comic) {
+    const path = (comic === 'latest') ? '' : `${comic}/`;
+    const endpoint = `${DOMAIN}/${path}/${PATH}`;
+    const xkcd = await axios(endpoint);
+    response = xkcd.data;
+  }
+  res.send(response);
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# XKCD Proxy
+
+## Latest comic
+
+```
+https://xkcd.now.sh/?comic=latest
+```
+
+## Comic #542
+
+```
+https://xkcd.now.sh/?comic=542
+```
